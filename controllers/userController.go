74ae3da897dca5fefe41b1defea25afe1884f775
Vivek Prakash <alivevivek8@gmail.com>
2025-03-08 04:47:18 +0000

    Added all project files

---
+++ controllers/userController.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	helper "golang-jwt-auth/helpers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+func HashPassword()
+
+func VerifyPassword()
+
+func Signup() gin.HandlerFunc
+
+func Login()
+
+func GetUsers()
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
+}
---
+++ helpers/authHelper.go
@@ -0,0 +1,9 @@
+package helper
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+
+}
